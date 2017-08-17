959fd6c354679b50ad212d65c5891e3853be6e37
wataru <0ky3r43m78m090b@gmail.com>
2017-08-18 07:59:20 +0900

    無効なバケットを指定された場合に400を返す

--- lambda_function.py
+++ lambda_function.py
@@ -28,21 +28,34 @@ def get_waterlevel(target_key):
     response = client.get_object(Bucket=bucket, Key=target_key)
 
     body = response['Body'].read().decode('utf-8')
-    json_dict = json.loads(body)
+    #json_str = json.loads(body)
         
-    return json_dict
+    return body
+
+def set_response_body(status_code, body):
+    headers = {}
+    headers['Content-Type'] = 'application/json'
+
+    res_body = {}
+    res_body['statusCode'] = status_code
+    res_body['headers'] = headers
+    res_body['body'] = body
+    
+    return res_body
 
 def lambda_handler(event, context):
+    print(event)
     # prefix用に年月取得
     now = datetime.datetime.now()
     year = str(now.strftime('%Y'))
     month = str(now.strftime('%m'))
     
+    params = event['queryStringParameters']
     # パラメータが不正な場合のデフォルトを荒川に
-    if (set(event) >= {'country', 'prefectures', 'river'}):
-        country = event['country'] if len(event['country']) != 0 else 'japan'
-        prefectures = event['prefectures'] if len(event['prefectures']) != 0 else 'tokyo'
-        river = event['river'] if len(event['river']) != 0 else 'arakawa'
+    if (set(params) >= {'country', 'prefectures', 'river'}):
+        country = params['country'] if len(params['country']) != 0 else 'japan'
+        prefectures = params['prefectures'] if len(params['prefectures']) != 0 else 'tokyo'
+        river = params['river'] if len(params['river']) != 0 else 'arakawa'
         prefix = target + '/' + country + '/' + prefectures + '/' + river + '/' + year + '/' + month + '/'
     else:
         prefix = 'waterLevel/japan/tokyo/arakawa/' + year + '/' + month + '/'
@@ -51,10 +64,9 @@ def lambda_handler(event, context):
         # 最新のファイル名を取得
         target_key = get_latest_keyname(prefix)
         # ファイル名をkeyとしてS3からデータ取得
-        json_dict = get_waterlevel(target_key)
+        json_str = get_waterlevel(target_key)
         
-        return json_dict
+        return set_response_body(200, json_str)
     except Exception as e:
         print(e)
-        # print('Error getting object {} from bucket {}. Make sure they exist and your bucket is in the same region as this function.'.format(key, bucket))
-        return 'InternalServerError'
+        return set_response_body(400, 'Bad Request')
